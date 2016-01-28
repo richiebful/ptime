e56afc6a8ee48c1b9357486a9d13bb55a0c96f38
Richard Fulop <richardbfulop@gmail.com>
2016-01-27 22:51:26 -0500

    Version 1, post testing

--- main.go
+++ main.go
@@ -19,18 +19,18 @@ func validZone(tz int) bool{
 	return (tz >= -12.0) && (tz <= 12.0)
 }
 
-func validZip(zip int) bool{
-	return (zip > 0) && (zip < 100000)
+func validZip(zip string) bool{
+	return len(zip) == 5
 }
 
 func main(){
 	todayDate, defZone := nowDate()
-	defDate := todayDate.Format("01/02/2006 -0700")
+	defDate := todayDate.Format("01/02/2006")
 	tz := flag.Int("tz", defZone, "Time zone of location")
 	dateString := flag.String("date", defDate, "Date of calculation")
 	long := flag.Float64("long", math.NaN() , "Longitude of position")
 	lat := flag.Float64("lat", math.NaN(), "Latitude of position")
-	zip := flag.Int("zip", -1, "Zip Code of location")
+	zip := flag.String("zip", "-1", "Zip Code of location")
 
 	flag.Parse()
 
@@ -40,30 +40,33 @@ func main(){
 	longF := validLongitude(*long)
 	zipF := validZip(*zip)
 	zoneF := validZone(*tz)
-	date, err := time.Parse("01/02/2006 -0700", *dateString)
-	fmt.Println(date, *zip)
+	date, err := time.Parse("01/02/2006", *dateString)
+	
 	if (err != nil){
 		fmt.Printf("Invalid date %s\n", *dateString)
+		return
+	}else if math.IsNaN(*lat) && math.IsNaN(*long) && !zipF {
+		fmt.Printf("No location defined\n")
 		return
 	}else if (latF || longF) && zipF {
 		fmt.Printf("Conflicting coordinates and zip\n")
 		return
-	}else if !latF && longF && *lat != math.NaN() {
+	}else if !latF && longF {
 		fmt.Printf("Invalid latitude, %f\n", lat)
 		return
-	}else if latF && !longF && *long != math.NaN(){
+	}else if latF && !longF {
 		fmt.Printf("Invalid longitude, %f\n", long)
 		return
-	}else if !latF && longF {
+	}else if math.IsNaN(*lat) && !math.IsNaN(*long) {
 		fmt.Printf("Missing latitude\n")
 		return
-	}else if latF && !longF {
+	}else if !math.IsNaN(*lat) && math.IsNaN(*long) {
 		fmt.Printf("Missing longitude\n")
 		return
 	}else if !zoneF {
 		fmt.Printf("Invalid time zone, %d\n", tz)
 		return
-	}else if !zipF && *zip != -1 {
+	}else if !zipF && *zip != "-1" {
 		fmt.Printf("Invalid zip, %d\n", zip)
 		return
 	}else if latF && longF {
@@ -71,9 +74,9 @@ func main(){
                 loc.long = *long
                 loc.tz = *tz
 	}else if zipF {
-		zipS := fmt.Sprintf("%d", *zip)
-		latitude, longitude, err := getCoords("/usr/share/weather-util/zctas", zipS)
+		latitude, longitude, err := getCoords("/usr/share/weather-util/zctas", *zip)
                 if (err != nil){
+			fmt.Printf("Zip code %s is not available\n", *zip)
                         return
                 }
 		loc.lat = latitude
--- tester.sh
+++ tester.sh
@@ -1,14 +1,4 @@
-$GOPATH/bin/ptime
-echo "SHOULD BE FAIL 1"
-$GOPATH/bin/ptime -tz -15
-echo "SHOULD BE FAIL 2"
-$GOPATH/bin/ptime -lat 40
-echo "SHOULD FAIL 3"
-$GOPATH/bin/ptime -lat -360
-echo "SHOULD FAIL 4"
-$GOPATH/bin/ptime -long -80
-echo "SHOULD FAIL 5"
-$GOPATH/bin/ptime -long 100
-echo "SHOULD FAIL 6"
-$GOPATH/bin/ptime -long 4959
-echo "SHOULD FAIL 7"
+$GOPATH/bin/ptime -long -9 -lat 9 -zip 15213
+echo "CONFLICTING TYPES"
+$GOPATH/bin/ptime -long -80 -lat 40 -date "01/29/2016"
+echo "TESTOR"
